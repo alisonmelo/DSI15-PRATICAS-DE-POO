6936f509f225a208ffe75a8204982780d3793dd6
adrianodmarco-spec <adrianodmarco@gmail.com>
2026-07-10 13:16:00 +0000

    Chapolim.java

---
+++ Desafio-Express/Chapolim.java
@@ -0,0 +1,24 @@
+class Heroi {
+    // Atributos (O que ela TEM)
+    String Nome;
+    int Vida;
+    // Constructor
+        public Heroi(String Nome,int Vida) {
+            this.Nome = Nome;
+            this.Vida = Vida;
+            
+    }
+}
+
+public class Chapolim {public static void main(String[] args){
+Heroi h1 = new Heroi ("Ainz Over D' Tempest", 16000 );
+
+    System.out.println("Heroi: " + h1.Nome +" VIDA: " + h1.Vida);  
+
+    Gabarge collector
+    // h1 = null;
+
+    
+    }
+}
+
