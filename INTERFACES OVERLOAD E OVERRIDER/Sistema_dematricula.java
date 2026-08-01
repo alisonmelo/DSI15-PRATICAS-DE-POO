52b00478a1447b1e9198862d0893ef6c332dfdef
pereira-sketch <pereiratch68@gmail.com>
2026-08-01 00:08:45 +0000

    Atividade 1

---
+++ INTERFACES OVERLOAD E OVERRIDER/Sistema_dematricula.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Pessoa {
+    protected String nome;
+    protected int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
+
+class Aluno extends Pessoa {
+    private String matricula;
+
+    public Aluno(String nome, int idade, String matricula) {
+        super(nome, idade);
+        this.matricula = matricula;
+    }
+}
+
+public class Sistema_dematricula {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do aluno: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite a idade do aluno: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.print("Digite a matrícula do aluno: ");
+        String matricula = scanner.nextLine();
+
+        Aluno aluno = new Aluno(nome, idade, matricula);
+
+        System.out.println("Aluno " + aluno.nome + " cadastrado com sucesso!");
+
+        scanner.close();
+    }
+}
